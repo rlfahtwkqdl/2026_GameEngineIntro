6e2692cee31ace6b9747933e3e59f0a7f3cf9c89
rlfhtwkqdl <hongjunsu87@gmail.com>
2026-03-19 15:35:46 +0900

    pppp는 내꺼 아님;;

--- unity hjs/Assets/playercontroller.cs
+++ unity hjs/Assets/playercontroller.cs
@@ -5,11 +5,29 @@ public class PlayerContreller : MonoBehaviour
 {
     private Vector2 moveInput;
     public float moveSpeed = 7f;
+    public float jumpforce = 7f;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
 
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpforce);
+        }
     }
 
     void Update()
@@ -22,6 +40,15 @@ public class PlayerContreller : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
+        if (moveInput.magnitude > 0)
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
+        }
         transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
+        
     }
 }
