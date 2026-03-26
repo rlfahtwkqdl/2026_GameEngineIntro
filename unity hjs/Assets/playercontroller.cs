393eead9f607db9b8d1290e45e5a7494368e2da4
rlfahtwkqdl <hongjunsu87@gmail.com>
2026-03-26 16:53:02 +0900

    타이틀 화면 홍준수

--- unity hjs/Assets/playercontroller.cs
+++ unity hjs/Assets/playercontroller.cs
@@ -1,10 +1,23 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PlayerContreller : MonoBehaviour
 {
     private Vector2 moveInput;
     public float moveSpeed = 7f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rd;
+    private Animator myAnimator;
+
+
+    private void Start()
+    {
+        rd = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetBool("move", false);
+    }
 
 
     public void OnMove(InputValue value)
@@ -12,6 +25,14 @@ public class PlayerContreller : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
+    
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rd.linearVelocity = new Vector2(rd.linearVelocity.x, jumpForce);
+        }
+    }
     void Update()
     {
         if (moveInput.x > 0)
@@ -23,5 +44,30 @@ public class PlayerContreller : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
         transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
+
+        if(moveInput.magnitude > 0)
+        {
+            myAnimator.SetBool("move", true);
+        }
+        else
+        {
+            myAnimator.SetBool("move", false);
+        }
+        transform.Translate(Vector3.right * moveSpeed * moveInput.x * Time.deltaTime);
+
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Death")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            SceneManager.LoadScene("PlayScene_" + collision.name);
+        }
+        
+    }
+
+    
 }
